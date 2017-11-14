bf787ee3565314192a49cd79a5ac88c53527c26e
mikakane <t.goto@chatbox-inc.com>
2017-11-14 15:24:19 +0900

    Dev (#9)
    * update
    
    * hoge
    
    * hoge
    
    * hoge
    
    * hoge
    
    * hoge
    
    * update
    
    * update
    
    * update

--- components/pages/Index/Detail.vue
+++ components/pages/Index/Detail.vue
@@ -12,11 +12,18 @@
     <div class="p-detail--mainArea">
       <div class="p-detail">
         <div class="p-detail--inner">
-          <h2 class="p-detail--subtitle">{{messages.subtitle}}</h2>
-          <p class="p-detail--body">{{messages.message}}</p>
-          <div class="p-detail--image" style="background-image: url('/img/creation02.jpg');"></div>
-          <br>
-          <div class="p-detail--image" style="background-image: url('/img/creation02.jpg');"></div>
+          <div class="p-detail--content1">
+            <h2 class="p-detail--subtitle">{{messages.subtitle}}</h2>
+            <p class="p-detail--body">{{messages.message}}</p>
+            <div class="p-detail--image" v-if='image1' :style="imageStyle1"></div>
+          </div>
+          <div class="p-detail--content2">
+            <div class="p-detail--image2" v-if='image2' :style="imageStyle2"></div>
+            <div class="p-detail--tags">
+              <p class="p-detail--tagItem">Web制作</p>
+              <p class="p-detail--tagItem">技術顧問</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -36,8 +43,43 @@
         default: true
       }
     },
+    data () {
+      return {
+        image1: null,
+        image2: null
+      }
+    },
     components: {
       PageLink
+    },
+    computed: {
+      imageStyle1 () {
+        if (this.image1) {
+          return {
+            backgroundImage: `url('${this.image1}')`
+          }
+        } else {
+          return {}
+        }
+      },
+      imageStyle2 () {
+        if (this.image2) {
+          return {
+            backgroundImage: `url('${this.image2}')`
+          }
+        } else {
+          return {}
+        }
+      }
+    },
+    async mounted () {
+      await new Promise((resolve) => {
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
+      this.image1 = this.messages.image1
+      this.image2 = this.messages.image2
     }
   }
 </script>
@@ -51,9 +93,17 @@
     @include c-container;
     &--inner{
       @include c-containerInner;
+      @include desktop{
+        display: flex;
+      }
     }
     &--title{
       @include c-title
+    }
+    &--content1{
+      @include desktop(){
+        width: 50%;
+      }
     }
     &--subtitle{
       @include c-caption
@@ -67,7 +117,43 @@
       background-size: cover;
       background-repeat: no-repeat;
     }
+    &--content2{
+      display: flex;
+      @include desktop(){
+        display: block;
+        width: 50%;
+      }
+    }
+    &--image2{
+      height: 20rem;
+      width: 60%;
+      background-size: cover;
+      background-position-y: 50%;
+      background-repeat: no-repeat;
+      @include desktop(){
+        height: 70rem;
+        width: 100%;
+      }
+    }
+    &--tags{
+      display: flex;
+      width: 40%;
+      justify-content: center;
+      flex-wrap: wrap;
+      @include desktop(){
+        width: 100%;
+      }
+
+    }
+    &--tagItem{
+      writing-mode: vertical-rl;
+      color: #C3504F;
+      font-weight: bold;
+      margin: 3rem 1rem;
+      height: 20rem;
+    }
   }
+  //画像の順番とかも無理あるかも Talking だけ別コンポーネントも検討
   .is-colored{
     .p-detail{
       &--titleArea{
