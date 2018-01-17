bcb94daf599c84bb910ffd5a31f4f3f5fbf6d1ea
yasui05821 <yasui05821@gmail.com>
2018-01-17 17:50:50 +0900

    <HEAD>の記述　'@nuxtjs/pwa'　のコメントアウト

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,17 @@ module.exports = {
   ** Headers
   ** Common headers are already provided by @nuxtjs/pwa preset
   */
-  head: {},
+  head: {
+    titleTemplate: '株式会社chatbox',
+    meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, minimum-scale=1, initial-scale=1, user-scalable=no' },
+        { hid: 'description', name: 'description', content: 'Meta description' },
+    ],
+    link: [
+        { rel: 'favicon', href: 'favicon.ico' }
+    ]
+  },
   css: [ ],
   /*
   ** Customize the progress-bar color
@@ -23,6 +33,6 @@ module.exports = {
   ** Modules
   */
   modules: [
-    '@nuxtjs/pwa'
+    // '@nuxtjs/pwa'
   ]
 }
