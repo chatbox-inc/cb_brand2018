2db3b93c6f93969237911ad3d16dd781bc4a514f
Kazuki Tobita <kazukiti201@gmail.com>
2018-01-22 14:07:32 +0900

    左上のラベルを修正

--- components/Header.vue
+++ components/Header.vue
@@ -16,20 +16,20 @@
       position: fixed;
       top: 0;
       left: 0;
-      border: 40px solid transparent;
-      border-top: 40px solid #c2504f;
-      border-left: 40px solid #c2504f;
+      border: 30px solid transparent;
+      border-top: 30px solid #c2504f;
+      border-left: 30px solid #c2504f;
       &::after {
         content: "";
         display: inline-block;
-        width: 25px;
-        height: 25px;
+        width: 20px;
+        height: 20px;
         background: url("/images/icon_chatbox-white.svg") no-repeat;
         background-size: contain;
         background-position: center;
         position: absolute;
-        top: -30px;
-        left: -30px;
+        top: -20px;
+        left: -25px;
       }
       @include desktop(){
         border: 50px solid transparent;
