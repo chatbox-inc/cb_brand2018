30319f4ad7a1a175a47e9e88b3e570bcfe91f870
Kazuki Tobita <kazukiti201@gmail.com>
2018-02-13 14:46:00 +0900

    Contact だいたい完成

--- components/pages/Contact/index.vue
+++ components/pages/Contact/index.vue
@@ -43,23 +43,25 @@
                     </span>
                 </div>
                 <form class="p-contact__formContact">
-                    <div class="p-contact__checkBoxWrapper">
-                        <input type="checkbox" id="web" name="web" class="p-contact__checkBox">
-                        <label for="web" class="p-contact__checkBoxLabel">Web制作</label>
+                    <div class="p-contact__contentWrapper">
+                        <div class="p-contact__content">
+                            <input type="checkbox" id="web" name="web" class="p-contact__checkBox">
+                            <label for="web" class="p-contact__checkBoxLabel">Web制作</label>
+                        </div>
+                        <div class="p-contact__content">
+                            <input type="checkbox" id="advise" name="advise" class="p-contact__checkBox">
+                            <label for="advise" class="p-contact__checkBoxLabel">技術顧問</label>
+                        </div>
+                        <div class="p-contact__content">
+                            <input type="checkbox" id="event" name="event" class="p-contact__checkBox">
+                            <label for="event" class="p-contact__checkBoxLabel">イベント・スクール</label>
+                        </div>
+                        <div class="p-contact__content">
+                            <input type="checkbox" id="etc" name="etc" class="p-contact__checkBox">
+                            <label for="etc" class="p-contact__checkBoxLabel">その他</label>
+                        </div>
                     </div>
-                    <div class="p-contact__checkBoxWrapper">
-                        <input type="checkbox" id="advise" name="advise" class="p-contact__checkBox">
-                        <label for="advise" class="p-contact__checkBoxLabel">技術顧問</label>
-                    </div>
-                    <div class="p-contact__checkBoxWrapper">
-                        <input type="checkbox" id="event" name="event" class="p-contact__checkBox">
-                        <label for="event" class="p-contact__checkBoxLabel">イベント・スクール</label>
-                    </div>
-                    <div class="p-contact__checkBoxWrapper">
-                        <input type="checkbox" id="etc" name="etc" class="p-contact__checkBox">
-                        <label for="etc" class="p-contact__checkBoxLabel">その他</label>
-                    </div>
-                    <div>Web制作に関するお問い合わせでは、見積り相談にも対応しておりますので内容にその旨を記述いただきご連絡ください。</div>
+                    <div class="p-contact__comment">Web制作に関するお問い合わせでは、見積り相談にも対応しておりますので内容にその旨を記述いただきご連絡ください。</div>
                     <div class="p-contact__textBoxWrapper">
                         <label for="name" class="p-contact__textBoxLabel">名前</label>
                         <input type="text" id="name" name="name" class="p-contact__textBox">
@@ -72,11 +74,11 @@
                         <label for="title" class="p-contact__textBoxLabel">件名</label>
                         <input type="text" id="title" name="title" class="p-contact__textBox">
                     </div>
-                    <div class="p-contact__textBoxWrapper">
-                        <label for="content" class="p-contact__textBoxLabel">内容</label>
-                        <textarea id="content" name="content" class="p-contact__textBox"></textarea>
+                    <div class="p-contact__textAreaWrapper">
+                        <label for="content" class="p-contact__textAreaLabel">内容</label>
+                        <textarea id="content" name="content" class="p-contact__textArea"></textarea>
                     </div>
-                    <button type="submit">送信</button>
+                    <button type="submit" class="p-contact__submit">送信する</button>
                 </form>
             </div>
         </div>
@@ -207,6 +209,136 @@
             border: 1px solid #CCCCCC;
             background-color: #FAF7F7;
             resize: none;
+        }
+
+        &__contentWrapper {
+            max-width: 700px;
+            width: 100%;
+            margin: 0 auto;
+        }
+
+        &__content {
+            display: inline-block;
+            width: 270px;
+            height: 60px;
+            margin-bottom: 30px;
+            border: 1px solid #C3504F;
+            border-radius: 5px;
+            color: #C3504F;
+            text-align: left;
+            line-height: 60px;
+            transition: all 0.3s;
+
+            &:nth-child(odd) {
+                margin-right: 15px;
+            }
+
+            &:nth-child(even) {
+                margin-left: 15px;
+            }
+
+            &:hover {
+                background: #C3504F;
+                color: #fff;
+            }
+        }
+
+        &__checkBox {
+            margin-left: 15px;
+        }
+
+        &__checkBoxLabel {
+            margin-left: 15px;
+        }
+
+        &__comment {
+            width: 100%;
+            color: #c3504f;
+            margin-bottom: 30px;
+        }
+
+        &__textBoxWrapper, &__textAreaWrapper {
+            margin-bottom: 20px;
+            text-align: left;
+        }
+
+        &__textBoxLabel {
+            display: inline-block;
+            margin-bottom: 5px;
+        }
+
+        &__textBox {
+            width: 100%;
+            height: 30px;
+            padding-left: 5px;
+            background: #f7f7f7;
+            border: 1px solid #ccc;
+            outline: none;
+        }
+
+        &__textArea {
+            width: 100%;
+            height: 200px;
+            padding-left: 5px;
+            padding-top: 5px;
+            background: #f7f7f7;
+            border: 1px solid #ccc;
+            resize: none;
+            outline: none;
+        }
+
+        &__submit {
+            display: inline-block;
+            margin-bottom: 10px;
+            padding: 2rem 1rem;
+            width:100%;
+            text-align: center;
+            border-radius: 4px;
+            border: 0;
+            color: #FFF;
+            font-size: 1.5rem;
+            font-weight: bold;
+            background:#C3504F;
+            text-decoration: none;
+            position:relative;
+            transition: all .5s;
+            outline: none;
+            cursor: pointer;
+            @include desktop {
+                font-size: 1.5rem;
+                line-height: 1.5;
+                font-weight: normal;
+                width: 45%;
+                letter-spacing: 2px;
+            }
+            &:hover {
+                opacity: .8;
+                transition: all .5s;
+                &::before {
+                    right: .2rem;
+                    transition: all .5s;
+                }
+            }
+            &::before {
+                content: "";
+                display: inline-block;
+                width: 1.4rem;
+                height: 1.4rem;
+                background-image: url("/images/icon_arrow-white.svg");
+                background-repeat: no-repeat;
+                background-size: contain;
+                background-position: center;
+                position: absolute;
+                right: .7rem;
+                top: 50%;
+                transform: translateY(-50%);
+                transition: all .5s;
+                @include desktop {
+                    width: 2rem;
+                    height: 2rem;
+                    right: 1rem;
+                }
+            }
         }
     }
 
