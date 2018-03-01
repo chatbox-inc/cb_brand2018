eded5820f59e81241a82f271a77a047b3453ce6b
yasui05821 <yasui05821@gmail.com>
2018-03-01 22:26:39 +0900

    webクリップアイコン設置

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,8 @@ module.exports = {
     ],
     link: [
         { rel: 'shortcut icon', href: '/favicon.ico' },
-        { rel:'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'}
+        { rel:'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css'},
+        { rel:'apple-touch-icon-precomposed', href: 'https://chatbox-inc.com/'}
     ]
   },
   css: [ ],
